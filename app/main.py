98867d3f58cea83f7585707d0dfd4250686c6fec
sathish <sathishakash30@gmail.com>
2026-06-05 12:03:37 +0530

    employee url added in main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.routes.authroutes import router as authroutes
+from app.routes.employeeroutes import router as employeeroutes
 
 
 app = FastAPI()
 
 app.include_router(authroutes)
+app.include_router(employeeroutes)
